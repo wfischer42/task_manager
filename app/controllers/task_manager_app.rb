46aab8a40f21f336ef4830af982f83da8bddb2e1
William Fischer <william.fischer42@gmail.com>
2018-09-30 22:34:13 -0600

    First (and only) commit

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,46 @@
+require_relative '../models/task.rb'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path('..', __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = Task.all
+    erb :index
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+
+  set :method_override, true
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  post '/tasks' do
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
+  end
+end
